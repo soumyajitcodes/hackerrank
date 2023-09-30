7879c80a1d1b926b215d34f957ff65a9353995c8
Soumyajit <soumyajit091@outlook.com>
2023-09-30 19:14:36 +0530

    restructuring

--- java-programming/src/introduction/Welcome.java
+++
@@ -1,8 +0,0 @@
-package introduction;
-
-public class Welcome {
-    public static void main(String[] args) {
-        System.out.println("Hello, World.");
-        System.out.println("Hello, Java.");
-    }
-}
